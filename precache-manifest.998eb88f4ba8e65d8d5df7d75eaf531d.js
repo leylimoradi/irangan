7cc461f365334be945f0d73cbc72cc804347bbe4
Leyli Moradi <l.moradi@Gan.local>
2020-09-07 15:20:21 +0430

    Updates

---
+++ precache-manifest.998eb88f4ba8e65d8d5df7d75eaf531d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdf499500cc159cfd24c1ad3f08aab8",
+    "url": "/irangan/index.html"
+  },
+  {
+    "revision": "0e12b979041ceae2d731",
+    "url": "/irangan/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4179d8092b7abf7080a2",
+    "url": "/irangan/static/js/2.059978d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/irangan/static/js/2.059978d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e12b979041ceae2d731",
+    "url": "/irangan/static/js/main.01440d35.chunk.js"
+  },
+  {
+    "revision": "e636d508a9ab19a749e7",
+    "url": "/irangan/static/js/runtime-main.6788a2e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/irangan/static/media/logo.5d5d9eef.svg"
+  }
+]);
