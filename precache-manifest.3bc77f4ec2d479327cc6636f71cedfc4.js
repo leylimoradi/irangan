d5e723b1683cd07afcb871c594c6074ae108ff95
Leyli Moradi <l.moradi@Gan.local>
2020-09-08 16:18:29 +0430

    Updates

---
+++ precache-manifest.3bc77f4ec2d479327cc6636f71cedfc4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c9e5abf40f80ea430e8b9da20594d8",
+    "url": "/irangan/index.html"
+  },
+  {
+    "revision": "f9b14b290d630c435f84",
+    "url": "/irangan/static/css/main.10d07281.chunk.css"
+  },
+  {
+    "revision": "fdba643428fe1fc5ba02",
+    "url": "/irangan/static/js/2.c8c7b86a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/irangan/static/js/2.c8c7b86a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b14b290d630c435f84",
+    "url": "/irangan/static/js/main.466edeca.chunk.js"
+  },
+  {
+    "revision": "e636d508a9ab19a749e7",
+    "url": "/irangan/static/js/runtime-main.6788a2e2.js"
+  },
+  {
+    "revision": "80470e5cc0dea2d75af2700d8c35ba6a",
+    "url": "/irangan/static/media/IRANSansMobile(FaNum).80470e5c.ttf"
+  },
+  {
+    "revision": "c6f46673e2449eb071e2c098c5190c2a",
+    "url": "/irangan/static/media/IRANSansWeb.c6f46673.woff2"
+  },
+  {
+    "revision": "df14582918ca379a280e453bb3cc6ba5",
+    "url": "/irangan/static/media/IRANSansWeb.df145829.woff"
+  },
+  {
+    "revision": "e43cfbc1a67d90e910398ded8345cd32",
+    "url": "/irangan/static/media/IRANSansWeb.e43cfbc1.eot"
+  },
+  {
+    "revision": "2d81d1b5e13d6e98688cfa5b1fa33140",
+    "url": "/irangan/static/media/logoIrangan.2d81d1b5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/irangan/precache-manifest.37bc0c58d7c227b2fcf6a5ec18252436.js"
+  "/irangan/precache-manifest.3bc77f4ec2d479327cc6636f71cedfc4.js"
 );
 
 self.addEventListener('message', (event) => {
